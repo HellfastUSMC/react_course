de88fdf5e7e1f89ed9532ef5fe420f2c9bc24900
HellfastUSMC <4epenki@gmail.com>
2022-08-29 18:03:57 +0300

    added some mongooses

--- part3/blog_list/controller/blogs.js
+++ part3/blog_list/controller/blogs.js
@@ -9,15 +9,35 @@ const Blog = require('../models/models')
 //     })
 // })
 
+//get
 blogsRouter.get('', async (request, response) => {
   const blogs = await Blog.find({})
   response.json(blogs)
 })
 
+//get specific
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  response.json(blog)
+})
+
+//post
 blogsRouter.post('', async (request, response, next) => {
   const blog = new Blog(request.body)
   await blog.save()
   response.status(201).json(blog)
+})
+
+//patch
+blogsRouter.patch('/:id', async (request, response, next) => {
+  let u_blog = await Blog.findByIdAndUpdate(request.params.id, request.body, { returnDocument: 'after' })
+  response.status(200).json(u_blog)
+})
+
+//delete
+blogsRouter.delete('/:id', async (request, response, next) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204)
 })
 
 module.exports = blogsRouter
